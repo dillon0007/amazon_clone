49e1deb9e7fcd4f4577f07c823a76b85e1de192a
dillon0007 <dillongirkar30@gmail.com>
2022-07-04 00:50:28 +0530

    navigation bar half completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import './App.css';
+import Header from './Header';
+
 
 function App() {
   return (
     //BEM
     <div className="App">
-      <h1> Hello World</h1>
-      {/* {Headers} */}
+      <Header/>
+      
       {/* {Home} */}
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,36 @@
 import React from 'react'
-
-function header() {
+import "./Header.css";
+import SearchIcon from '@mui/icons-material/Search';
+function Header() {
   return (
-    <div className='header'>header</div>
+    <div className="header">
+        <img className='header__logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/2560px-Amazon_logo.svg.png'></img>
+        <div className='header__search'>
+            <input className='header_searchInput' type="text"/>
+            <SearchIcon className="header__searchIcon"/>
+        </div>
+        <div className='header__nav'>
+            <div className='header__option'>
+                <span className='header__optionLineOne'>Hello Guest</span>
+                <span className='header__optionLineOne'>Sign In</span>
+
+
+            </div>
+            <div className='header__option'>
+            <span className='header__optionLineOne'>Returns</span>
+            <span className='header__optionLineOne'>Orders</span>
+
+
+            </div>
+            <div className='header__option'>
+            <span className='header__optionLineOne'>Your </span>
+            <span className='header__optionLineOne'>Prime</span>
+
+
+            </div>
+        </div>
+        </div>
   )
 }
 
-export default header
+export default Header
